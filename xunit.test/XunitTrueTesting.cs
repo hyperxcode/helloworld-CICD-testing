2b84d980a529a55d2d4bfd99609e389fed86657f
chen.ye <277246252@qq.com>
2020-09-29 17:03:54 +0800

    add xunit for sonarcloud

--- xunit.test/XunitTrueTesting.cs
+++ xunit.test/XunitTrueTesting.cs
@@ -1,3 +1,4 @@
+using helloworld;
 using System;
 using System.Reflection;
 using Xunit;
@@ -7,15 +8,46 @@ namespace helloworldxunit.test
     public class XunitTrueTesting
     {
         [Fact]
-        public void IsTrueAndTrue()
+        public void TrueAndTrue()
         {
             Assert.True(true, "It is true");
         }
 
         [Fact]
-        public void IsTrueButFalse()
+        public void TrueButFalse()
         {
             //Assert.True(false, "It was false");
+        }
+
+
+        [Fact]
+        public void MathMethodInt()
+        {
+            MathMethod mathMethod = new MathMethod();
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.NULL, 1, 2) == 0, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.ADD, 1, 2) == 3, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.MINUS, 4, 2) == 2, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.MULTIPLY, 4, 2) == 8, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.DEVIDE, 2, 2) == 1, "It is true");
+        }
+
+        [Fact]
+        public void MathMethodDouble()
+        {
+            MathMethod mathMethod = new MathMethod();
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.NULL, 1.0, 2.0) == 0, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.ADD, 1.0, 2.0) == 3.0, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.MINUS, 4.0, 2.0) == 2.0, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.MULTIPLY, 4.0, 2.0) == 8.0, "It is true");
+            Assert.True(mathMethod.Opr(MathMethod.ENUM_OPR.DEVIDE, 2.0, 2.0) == 1.0, "It is true");
+        }
+
+        [Fact]
+        public void MathMethod2()
+        {
+            MathMethod2 mathMethod = new MathMethod2();
+            Assert.True(mathMethod.SomeCaculate(MathMethod.ENUM_OPR.MINUS) == 2, "It is true");
+
         }
     }
 }
